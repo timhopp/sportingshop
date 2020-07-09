6de699592a77730c643708546d8e52a0fe4c5fd9
tim hopp <timothyjhopp@gmail.com>
2020-07-09 14:05:06 -0600

    added item model

--- app/Models/Item.js
+++ app/Models/Item.js
@@ -1,6 +1,7 @@
-
+let id = 100
 export default class Item {
   constructor(data){
+    this.id = id++
     this.title = data.title 
     this.description = data.description
     this.img = data.img 
@@ -8,7 +9,17 @@ export default class Item {
   }
 
   get Template(){
+    return `
+  <div class="col-3 card bg-secondary text-light p-3 m-4">
+        <h2>${this.title}</h2>
+        <h3>${this.description}</h3>
+        <img class="img-fluid src="${this.img}">
+        <button class="btn btn-danger" onclick="app.itemsController.deleteItem(
+          '${this.id}')">Delete</button>
 
+    </div>
+  `
+  
   }
 
 }
