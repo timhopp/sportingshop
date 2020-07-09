2c94891bfc0246bfb4d3ff1f6ce4ec5dd6580425
tylermarshall7 <tyler.marshall7@gmail.com>
2020-07-09 14:08:44 -0600

    itemController (in progress)

--- app/Controllers/ItemsController.js
+++ app/Controllers/ItemsController.js
@@ -1,7 +1,27 @@
 import ItemsService from "../Services/ItemsService.js"
 
+
+function _draw() {
+  let template = ""
+  let items = _store.State.items
+  items.forEach(item => template += item.template)
+  document.getElementById("items").innerHTML = template
+}
+
+
 export default class ItemsController {
   constructor(){
     console.log('Hi Item Cont')
+    _draw()
+  }
+
+
+  addItem(event) {
+    event.preventDefault();
+    let formData = event.target
+
+    // raw item data inputs //
+
+    _itemsService.addItem(raw)
   }
 }
